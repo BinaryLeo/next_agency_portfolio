d352a917683142cb240e8aa17b8a9f140c97680e
binaryleo <leomouracss@fastmail.com>
2021-11-30 23:40:01 -0300

    Product's page

---
+++ nextjs-agency-page/pages/products/[name].js
@@ -0,0 +1,56 @@
+import { data } from "../../data";
+import styles from "../../styles/Product.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
+
+const Product = ({ product }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Digital Design for Your Product</title>
+        <meta
+          name="description"
+          content="Web Design, App Development, Content Creation Agency Near NY"
+        />
+      </Head>
+      <div className={styles.cardL}>
+        {product.images.map((img) => (
+          <div key={img.id} className={styles.imgContainer}>
+            <Image src={img.url} layout="fill" objectFit="cover" alt="" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.cardS}>
+        <h1 className={styles.title}>{product.title}</h1>
+        <p className={styles.desc}>{product.longDesc}</p>
+        <button className={styles.button}>
+          <Link href="/contact">Contact</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const products = data;
+  const paths = products.map((item) => {
+    return {
+      params: { name: item.name },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const name = ctx.params.name;
+  const product = data.filter((item) => item.name === name)[0];
+  return {
+    props: { product },
+  };
+};
+
+export default Product;
