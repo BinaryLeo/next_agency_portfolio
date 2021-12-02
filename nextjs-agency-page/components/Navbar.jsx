409499188e0c8a625f9613d187a8f9acd6c1e993
binaryleo <leomouracss@fastmail.com>
2021-12-02 00:36:44 -0300

    Media Query , Layout Adj.  and Hamburguer menu

--- nextjs-agency-page/components/Navbar.jsx
+++ nextjs-agency-page/components/Navbar.jsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
-import styles from "../styles/Navbar.module.css"
-import Link from "next/link"
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
-    return (
-        <div className={styles.container}>
-         <Link href="/">SCOOTER</Link>
-         <ul className={styles.list}>
-         <li className={styles.listItem}> <Link href="/products/design">DESIGN</Link></li>
-         <li className={styles.listItem}> <Link href="/products/development">DEVELOPMENT</Link></li>
-         <li className={styles.listItem}> <Link href="/products/production">PRODUCTION</Link></li>
-         <li className={styles.listItem}> <Link href="/products/photography">PHOTOGRAPHY</Link></li>
-         <li className={styles.listItem}> <Link href="contact">CONTACT</Link></li>
-         </ul>
-         <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={styles.container}>
+      <Link href="/">SCOOTER</Link>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          {" "}
+          <Link href="/products/design">DESIGN</Link>
+        </li>
+        <li className={styles.listItem}>
+          {" "}
+          <Link href="/products/development">DEVELOPMENT</Link>
+        </li>
+        <li className={styles.listItem}>
+          {" "}
+          <Link href="/products/production">PRODUCTION</Link>
+        </li>
+        <li className={styles.listItem}>
+          {" "}
+          <Link href="/products/photography">PHOTOGRAPHY</Link>
+        </li>
+        <li className={styles.listItem}>
+          {" "}
+          <Link href="/contact">CONTACT</Link>
+        </li>
+      </ul>
+      <div className={styles.hamburger} onClick={() => setOpen(!open)}>
         <div className={styles.line} />
         <div className={styles.line} />
         <div className={styles.line} />
       </div>
-      <ul onClick={()=>setOpen(false)} className={styles.menu} style={{ right: open ? "0px" : "-50vw" }}>
+      <ul
+        onClick={() => setOpen(false)}
+        className={styles.menu}
+        style={{ right: open ? "0px" : "-50vw" }}
+      >
         <li className={styles.menuItem}>
           <Link href="/">HOME</Link>
         </li>
@@ -38,7 +57,7 @@ const Navbar = () => {
           <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
-        </div>
-    )
-}
-export default Navbar
+    </div>
+  );
+};
+export default Navbar;
